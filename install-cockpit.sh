2adc7aa5956b77aa0cc4815d180fda8bf07eef36
IMNOTIKE <simone2002.oliva@gmail.com>
2022-09-24 11:22:56 +0200

    Update install-cockpit.sh

--- install-cockpit.sh
+++ install-cockpit.sh
@@ -1,32 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Installazione di Cockpit!"
 
 var=''
 pvar=''
-plugins_arr=("cockpit-machines", "cockpit-podman", "podman", "cockpit-sensors", "lm-sensors", "cockpit-packagekit", "cockpit-networkmanager", "cockpit-storaged", "cockpit-certificates")
+plugins_arr=(cockpit-machines cockpit-podman podman cockpit-sensors lm-sensors cockpit-packagekit cockpit-networkmanager cockpit-storaged cockpit-certificates)
 
 echo "Il modulo base di cockpit sta per essere installato"
 apt install cockpit -y
 
-until [ $var == S || $var == N ]
+until [ "$var" == S ] || [ "$var" == N ]
 do
-read -p "Vuoi visualizzare i plugin aggiuntivi?" var
+  read -r -p "Vuoi visualizzare i plugin aggiuntivi?" var
 
-if [ $var == S ]
-then
-  for str in ${plugins_arr[@]}; do
-    echo ${plugins_arr[@} 
-    read -p "Installare? " pvar
-    if [ $pvar == S ]
-      then 
-        apt install ${plugins_arr[@]}
-    elif [$pvar == N ]
-      then
-        echo "Il modulo ${plugins_arr[@]} non verrà installato"
+  if [ "$var" == S ]
+    then
+      for str in "${plugins_arr[@]}"
+      do
+        echo "$str" 
+        read -r -p "Installare? " pvar
+        if [ "$pvar" == S ]
+          then 
+            apt install "$str"
+        elif [ "$pvar" == N ]
+          then
+            echo "Il modulo $str non verrà installato"
+        fi
+      done
+  else
+       echo "I plugin aggiuntivi non verranno installati"
     fi
-else
-  then
-    echo "I plugin aggiuntivi non verranno installati"
-fi
 done
