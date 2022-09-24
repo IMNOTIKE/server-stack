06f476a754d2b1f1f08d8bd1ebb46739d4c80bbe
IMNOTIKE <simone2002.oliva@gmail.com>
2022-09-24 12:08:03 +0200

    Update install-cockpit.sh

--- install-cockpit.sh
+++ install-cockpit.sh
@@ -3,32 +3,40 @@
 echo "Installazione di Cockpit!"
 var=''
 pvar=''
+ORANGE='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
 plugins_arr=(cockpit-machines cockpit-podman podman cockpit-sensors lm-sensors cockpit-packagekit cockpit-networkmanager cockpit-storaged cockpit-certificates)
 
-echo "Il modulo base di cockpit sta per essere installato"
+echo -e "Il modulo base di ${ORANGE}cockpit${NC} sta per essere installato"
 apt install cockpit -y
 systemctl enable --now cockpit.socket
 sudo usermod -aG sudo "$USER"
+clear
 
 until [ "$var" == S ] || [ "$var" == N ] || [ "$var" == s ] || [ "$var" == n ]
 do
-  read -r -p "Vuoi visualizzare i plugin aggiuntivi?" var
+  read -r -p "$(echo -e "${ORANGE}Vuoi visualizzare i plugin aggiuntivi?") " var
 
-  if [ "$var" == S ]
+  if [ "$var" == S ] || [ "$var" == s ]
     then
       for str in "${plugins_arr[@]}"
       do
-        echo "$str" 
-        read -r -p "Installare? " pvar
-        if [ "$pvar" == S ]
+        sleap 0.5s
+        clear 
+        read -r -p "Installare $(echo -e "${ORANGE}$str${NC}")? " pvar
+        if [ "$pvar" == S ] || [ "$pvar" == s ] 
           then 
-            apt install "$str"
-        elif [ "$pvar" == N ]
+            apt install "$str" -y
+            clear
+        elif [ "$pvar" == N ] || [ "$pvar" == n ]
           then
-            echo "Il modulo $str non verrà installato"
+            clear
+            echo  -e "Il modulo ${ORANGE}$str${NC} ${RED}non verrà installato"
         fi
       done
   else
-       echo "I plugin aggiuntivi non verranno installati"
+       clear
+       echo -e "I plugin aggiuntivi ${RED}non verranno installati"
     fi
 done
